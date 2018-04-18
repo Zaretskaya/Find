4e94995baba7d3b23ba1b37fe4f69b7cb11dc090
Zaretskaya.es <liza.9999@bk.ru>
2018-04-18 19:29:16 +0300

    test

--- src/com/company/Find.java
+++ src/com/company/Find.java
@@ -8,12 +8,18 @@ public class Find {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
 
-    private static String function(String path, String find, boolean r){
+
+    private static String function(String path, String find, boolean r) throws Exception {
+
+        FileWriter fw = new FileWriter("C:\\Users\\Acer\\IdeaProjects\\Find\\output.txt");
+
         File f = new File (path);
         String[] list = f.list();
         for (String file: list) {
             if (file.equals(find)){
-                return path+file+ " file found";
+                String result = path+"\\"+file+ " file found";
+                fw.write(result);
+                return result;
             }
             else {
                 File t = new File (file);
@@ -23,10 +29,39 @@ public class Find {
                 }
             }
         }
+        fw.write("not found");
+        fw.close();
         return "not found";
     }
 
-    public static void main(String[] args) {
+
+    public static String action() throws Exception{
+        String commandLine = in.nextLine();
+        String[] list = commandLine.split(" ");            //find [-r] [-d directory] filename.txt
+
+        String fileName = list[list.length - 1];
+        boolean r = false;
+        String d = "C:\\Users\\Acer\\IdeaProjects\\Find";
+
+        for (int i = 1; i < list.length; i++) {
+            if (list[i].equals("-r"))
+                r = true;
+            if (list[i].equals("-d"))
+                d = list[i+1];
+        }
+
+        return (function(d, fileName, r));
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        String result = action();
+        out.print(result);
+    }
+
+
+
+    /*public static void main(String[] args) {
         String commandLine = in.nextLine();
         String[] list = commandLine.split(" ");            //find [-r] [-d directory] filename.txt
 
@@ -44,4 +79,6 @@ public class Find {
         out.print(function(d, fileName, r));
 
     }
+*/
+
 }
--- src/com/company/FindTest.java
+++ src/com/company/FindTest.java
@@ -1,11 +1,43 @@
 package com.company;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class FindTest {
-    @org.junit.jupiter.api.Test
 
-    void main() {
+    private static void assertFileContent(String name, String expectedContent) {
+        StringBuilder content = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(name));
+            String curr;
+            while ((curr = br.readLine()) != null) {
+                content.append(curr);
+                content.append("\n");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        int length = content.length();
+        if (length > 0) content.deleteCharAt(length - 1);
+        assertEquals(expectedContent, content.toString());
+
+    }
+
+
+
+
+
+    @Test
+    void main() throws Exception {
+        String[] arg = {"find ", "-r ", " test.txt"};
+        Find.main(arg);
+        assertFileContent("output.txt", "exp.txt");
     }
 
 }
