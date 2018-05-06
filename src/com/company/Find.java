615cc92e5b45163d732250c289f5b22e6280ae17
Zaretskaya.es <liza.9999@bk.ru>
2018-05-06 23:06:06 +0300

    Done

--- src/com/company/Find.java
+++ src/com/company/Find.java
@@ -8,40 +8,14 @@ public class Find {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
 
-
-    private static String function(String path, String find, boolean r) throws Exception {
-
-        FileWriter fw = new FileWriter("C:\\Users\\Acer\\IdeaProjects\\Find\\output.txt");
-
-        File f = new File (path);
-        String[] list = f.list();
-        for (String file: list) {
-            if (file.equals(find)){
-                String result = path+"\\"+file+ " file found";
-                fw.write(result);
-                return result;
-            }
-            else {
-                File t = new File (file);
-                if (t.isDirectory() && r) {
-                   String newPath = path+"\\"+file;
-                   function(newPath, find, r);
-                }
-            }
-        }
-        fw.write("not found");
-        fw.close();
-        return "not found";
-    }
-
-
     public static String action() throws Exception{
+        Logic logic = new Logic();
         String commandLine = in.nextLine();
         String[] list = commandLine.split(" ");            //find [-r] [-d directory] filename.txt
 
         String fileName = list[list.length - 1];
         boolean r = false;
-        String d = "C:\\Users\\Acer\\IdeaProjects\\Find";
+        String d = "Find";
 
         for (int i = 1; i < list.length; i++) {
             if (list[i].equals("-r"))
@@ -50,7 +24,7 @@ public class Find {
                 d = list[i+1];
         }
 
-        return (function(d, fileName, r));
+        return (logic.function(d, fileName, r));
 
     }
 
@@ -67,7 +41,7 @@ public class Find {
 
         String fileName = list[list.length - 1];
         boolean r = false;
-        String d = "C:\\Users\\Acer\\IdeaProjects\\Find";
+        String d = "C:/Users/Acer/IdeaProjects/Find";
 
         for (int i = 1; i < list.length; i++) {
             if (list[i].equals("-r"))
