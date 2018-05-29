5ce5496ae8431950614f39205de31cea1eb0b531
Zaretskaya.es <liza.9999@bk.ru>
2018-05-29 11:31:26 +0300

    Не проверяйте

--- src/com/company/Find.java
+++ src/com/company/Find.java
@@ -8,14 +8,32 @@ public class Find {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
 
+    private static String function(String path, String find, boolean r) throws Exception {
+        FileWriter fw = new FileWriter("C:\\Users\\Acer\\IdeaProjects\\Find\\output.txt");
+
+        File f = new File(path);
+        String[] list = f.list();
+        for (String file : list) {
+            if (file.equals(find)) {
+                String result = path + "\\" + file + " file found";
+                fw.write(result);
+                return result;
+            } else {
+                File t = new File(file);
+            }
+        }
+
+        fw.write("not found");
+        fw.close();
+        return "not found";
+    }
     public static String action() throws Exception{
-        Logic logic = new Logic();
         String commandLine = in.nextLine();
         String[] list = commandLine.split(" ");            //find [-r] [-d directory] filename.txt
 
         String fileName = list[list.length - 1];
         boolean r = false;
-        String d = "Find";
+        String d = "C:\\Users\\Acer\\IdeaProjects\\Find";
 
         for (int i = 1; i < list.length; i++) {
             if (list[i].equals("-r"))
@@ -24,35 +42,19 @@ public class Find {
                 d = list[i+1];
         }
 
-        return (logic.function(d, fileName, r));
-
+        return (function(d, fileName, r));
     }
 
     public static void main(String[] args) throws Exception {
-        String result = action();
-        out.print(result);
-    }
+    String result = action();
+    out.print(result);
+}
+/*public static void main(String[] args) {
+         String commandLine = in.nextLine();
+         String[] list = commandLine.split(" ");            //find [-r] [-d directory] filename.txt
+         static void main(String[] args) {
+         out.print(function(d, fileName, r));
 
-
-
-    /*public static void main(String[] args) {
-        String commandLine = in.nextLine();
-        String[] list = commandLine.split(" ");            //find [-r] [-d directory] filename.txt
-
-        String fileName = list[list.length - 1];
-        boolean r = false;
-        String d = "C:/Users/Acer/IdeaProjects/Find";
-
-        for (int i = 1; i < list.length; i++) {
-            if (list[i].equals("-r"))
-                r = true;
-            if (list[i].equals("-d"))
-                d = list[i+1];
-        }
-
-        out.print(function(d, fileName, r));
-
-    }
-*/
-
+     }
++*/
 }
